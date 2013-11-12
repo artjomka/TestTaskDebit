f969e9c6d3570bf97903942afe84515b8cde1cb3
artjom <artjom.kalita@gmail.com>
2013-11-12 23:46:58 +0200

    HSQLDB server added

---
+++ src/main/java/lv/testtask/HSQLDbServer.java
@@ -0,0 +1,71 @@
+package lv.testtask;
+
+import org.hsqldb.Server;
+import org.hsqldb.persist.HsqlProperties;
+import org.hsqldb.server.ServerAcl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.SmartLifecycle;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class HSQLDbServer implements SmartLifecycle {
+
+    private final Logger LOG = LoggerFactory.getLogger(HSQLDbServer.class);
+    private HsqlProperties properties;
+    private Server server;
+    private boolean running = false;
+
+    public HSQLDbServer(Properties props) {
+        properties = new HsqlProperties(props);
+    }
+
+    @Override
+    public boolean isAutoStartup() {
+        return true;
+    }
+
+    @Override
+    public void stop(Runnable callback) {
+        stop();
+        callback.run();
+    }
+
+    @Override
+    public void start() {
+        if (server == null) {
+            LOG.info("Starting HSQLDB server");
+            server = new Server();
+
+            try {
+                server.setProperties(properties);
+                server.start();
+                running = true;
+            } catch (IOException e) {
+                LOG.error("Error starting server", e);
+            } catch (ServerAcl.AclFormatException e) {
+                LOG.error("Error starting server", e);
+            }
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (server != null) {
+            LOG.info("Stopping HSQLDB server");
+            server.stop();
+            running = false;
+        }
+    }
+
+    @Override
+    public boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    public int getPhase() {
+        return 0;
+    }
+}
--- src/main/java/lv/testtask/config/AppConfig.java
+++ src/main/java/lv/testtask/config/AppConfig.java
@@ -2,8 +2,9 @@ package lv.testtask.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = "lv.testtask.config", excludeFilters = @ComponentScan.Filter(value = MvcConfig.class, type = FilterType.CUSTOM))
 public class AppConfig {
 }
